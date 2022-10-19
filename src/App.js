6d2627a22429b4924c8aa26cbf9d0d6454767817
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2022-10-19 21:38:30 +0200

    .

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ import Destination from "./components/destination/destination";
 import Crew from "./components/crew/crew";
 
 function App() {
-  /*   //const [lotfy,setLotfy] = useState([]);
-  const [get, setGet] = useState(0);
-  const [lotfy, setLotfy] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000/api/v1/tasks")
-      .then((data) => setLotfy(data.data.tasks));
-  }, [get, lotfy]);
-  /* useEffect(()=>{
-    console.log(lotfy)
-  },[lotfy]) */
 
   return (
     <BrowserRouter>
